42cc2a08738091a4fdc1cb22b93ef4a123b49f1d
magdi14 <mohamed14magdi@gmail.com>
2020-03-27 09:27:53 +0200

    file for database connection created

---
+++ database/db.js
@@ -0,0 +1,18 @@
+/*
+var mongoose = require('mongoose');
+
+Here add your database connection then export the variable you made
+
+e.g:
+var db = mongoose.connect('mongoose://localhost:{port}/{your DB name}', { useNewUrlParser: true, useUnifiedTopology: true });
+db connection created successfully!
+
+PS: 
+    - Here the {port} variable you specified when you're creating your mongodb database.
+    - Then the next variable is the db name. 
+
+Then tou need to export it  
+e.g:   
+    module.exports = db;
+
+*/
