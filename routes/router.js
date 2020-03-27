51c3f61c82fc745141dd5fd871e3ca083c5badd4
magdi14 <mohamed14magdi@gmail.com>
2020-03-27 09:09:21 +0200

    created router initiation

---
+++ routes/router.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+
+/*
+var userCtrl = require('../app/controller/UsersController');
+
+Then here add your endpoints for the user controller like
+
+//Users
+router.route('users').get(userCtrl.index)  here index is a fn to get all users from db implemented in users controller.
+router.route('users/:id').get(userCtrl.show)  here show is a fn to get a user from db implemented in users controller with specific id represented in the url as shown.
+
+PS: you can accsess the id provided in url via request by params attribute.
+*/
+
+
+/* 
+After adding all your routes you must to export the router variable
+*/
+module.exports = router;
