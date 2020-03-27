c939d2dd7352cd5f14ac4748bfc73d34af876537
magdi14 <mohamed14magdi@gmail.com>
2020-03-27 09:57:53 +0200

    creates init model by mongoose

---
+++ database/models/User.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; //Defining a schema to create a model in mongoose.
+
+var user = new Schema({
+    // here add the collection entries for your models like name, email, etc ... 
+});
+
+//PS: By default, Mongoose adds an _id property to your schemas.
+
+//then export that variable as follows
+
+module.exports = mongoose.model('User', user); //here we are passing tha schema of the model
+
+//Check docs for mongoose here: https://mongoosejs.com/docs/index.html
