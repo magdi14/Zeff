ba32e2b29bb7ec1ebcc88091b51326d9ee8e55c6
magdi14 <mohamed14magdi@gmail.com>
2020-03-27 10:31:50 +0200

    specifying helpers

---
+++ app/helpers/helpers.js
@@ -0,0 +1,9 @@
+/*
+Here we add the helpers functions like:
+- Uploading files (png, jpg, pdf, etc...).
+- Handling resizing and dimensions of uploaded files.
+- Validations of data (emails, phones, etc ...).
+
+
+The most important part is exporting module then importing it in the controller you want.
+*/
