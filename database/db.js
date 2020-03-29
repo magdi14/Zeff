03c9578e62bdf3c4cf6c8a1f31ca77e4b8372e77
magdi14 <mohamed14magdi@gmail.com>
2020-03-29 04:26:40 +0200

    edits in db file after setting up .env file

--- database/db.js
+++ database/db.js
@@ -4,13 +4,17 @@ const mongoose = require('mongoose');
 Here add your database connection then export the variable you made
 
 e.g:
-const db = mongoose.connect('mongoose://{yr_db_Host}:{db_port}/{yr_db_name}', { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connect('mongoose://{yr_db_Host}:{db_port}/{yr_db_name}', { useNewUrlParser: true, useUnifiedTopology: true });
+OR
+    const db = mongoose.connect('mongodb://{username}:{password}@{yr_db_Host}:{db_port}/{yr_db_name}, { useNewUrlParser: true, useUnifiedTopology: true });
+and this if you have specific username and password for your db, so you will need to add them in .env file and access them here via process.env.DB_USERNAME and process.env.DB_PASSWORD,
+
 db connection created successfully!
 
 PS: 
-    - Here the {port} variable you specified when you're creating your mongodb database.
+    - Here the {db_port} variable you specified when you're creating your mongodb database.
     - Then the next variable is the db name. 
-
+    - Of course all of these vars must included in .env file to have a convenient access to it through the app.
 Then tou need to export it  
 e.g:   
     module.exports = db;
